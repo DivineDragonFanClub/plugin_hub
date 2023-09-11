e636cb64738b6b057a0e6c413a2ae0aec3dc7f64
Raytwo <raytwost@gmail.com>
2023-09-11 17:52:08 +0200

    Add template example

---
+++ 00 - Template/src/lib.rs
@@ -0,0 +1,61 @@
+/// This is called a proc(edural) macro. You use this to indicate that a function will be used as a hook.
+///
+/// Pay attention to the argument, offset.
+/// This is the address of the start of the function you would like to hook.
+/// This address has to be relative to the .text section of the game.
+/// If you do not know what any of this means, take the address in Ghidra and remove the starting ``71`` and the zeroes that follow it.
+/// Do not forget the 0x indicator, as it denotates that you are providing a hexadecimal value.
+#[skyline::hook(offset = 0x35faa80)]
+pub fn my_hook() { // Usually you'd add arguments similar to the function you are hooking here. For the sake of this example, we won't.
+    // This is the body of your hook. It will get ran instead of the function you hooked when the game tries to call it.
+
+    // You can trigger a panic whenever you want by calling this (declarative) macro.
+    // Since we installed a panic handler in the main function, it will be called and the message will be provided to it.
+    panic!("Bro :sob:");
+}
+
+/// The internal name of your plugin. This will show up in crash logs. Make it 8 characters long at max.
+#[skyline::main(name = "myplugin")]
+pub fn main() {
+    /// Install a panic handler for your plugin, allowing you to customize what to do if there's an issue in your code.
+    std::panic::set_hook(Box::new(|info| {
+        let location = info.location().unwrap();
+
+        // Some magic thing to turn what was provided to the panic into a string. Don't mind it too much.
+        // The message will be stored in the msg variable for you to use.
+        let msg = match info.payload().downcast_ref::<&'static str>() {
+            Some(s) => *s,
+            None => {
+                match info.payload().downcast_ref::<String>() {
+                    Some(s) => &s[..],
+                    None => "Box<Any>",
+                }
+            },
+        };
+
+        // This creates a new String with a message of your choice, writing the location of the panic and its message inside of it.
+        // Note the \0 at the end. This is needed because show_error is a C function and expects a C string.
+        // This is actually just a result of bad old code and shouldn't be necessary most of the time.
+        let err_msg = format!(
+            "Custom plugin has panicked at '{}' with the following message:\n{}\0",
+            location,
+            msg
+        );
+
+        // We call the native Error dialog of the Nintendo Switch with this convenient method.
+        // The error code is set to 69 because we do need a value, while the first message displays in the popup and the second shows up when pressing Details.
+        skyline::error::show_error(
+            69,
+            "Custom plugin has panicked! Please open the details and send a screenshot to the developer, then close the game.\n\0",
+            err_msg.as_str(),
+        );
+    }));
+
+    // This is what you call to install your hook(s).
+    // If you do not install your hook(s), they will just not execute and nothing will be done with them.
+    // It is common to install then in ``main`` but nothing stops you from only installing a hook if some conditions are fulfilled.
+    // Do keep in mind that hooks cannot currently be uninstalled, so proceed accordingly.
+    //
+    // A ``install_hooks!`` variant exists to let you install multiple hooks at once if separated by a comma.
+    skyline::install_hook!(my_hook);
+}
